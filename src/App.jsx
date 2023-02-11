f2064e170b37068fbb232e4171617545cda7b178
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 00:38:50 +0100

    Add id to items

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ function App() {
 
   const menuItems = [
     {
+      id: 123,
       name: "Coffee",
       description: "A fresh and delicious black coffee",
       price: 5.00
     },
     {
+      id: 124,
       name: "PBJ",
       description: "Peanut butter jelly time!",
       price: 3.45
     },
     {
+      id: 125,
       name: "Carrot cake",
       description: "Ordering here is a piece of cake",
       price: 6.20
