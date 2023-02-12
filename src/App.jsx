1dc145df2a69d727bd3f469194ad0068e8f27a4c
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-13 00:28:47 +0100

    Remove redeundant divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Outlet } from "react-router-dom";
 import CartOverlay from "./components/CartOverlay";
 import {useCart} from "./context/CartContext";
+import {classNames} from "./utils/classes";
 
 function App() {
   const cart = useCart();
   return (
     <div className="h-screen flex flex-col">
-      <div className="max-w-xl w-full flex-1 mx-auto pt-10 pb-80">
-        <div className="flex flex-col">
-          <div className="flex-1">
+      <div className={classNames("max-w-xl w-full flex-1",
+      "mx-auto pt-10 pb-32",
+      "flex flex-col")}>
             <Outlet />
-          </div>
-          <div className="flex-1">
-
-          </div>
-        </div>
       </div>
       <div className="fixed bottom-0 w-full bg-white">
           <CartOverlay cart={cart} />
