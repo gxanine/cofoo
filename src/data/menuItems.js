d60bda1ef6d5f85569271223484c3616fa8b0c8f
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 17:27:17 +0100

    Change data of id to string

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -1,18 +1,18 @@
 const menuItems = [
   {
-    id: 123,
+    id: "123",
     name: "Coffee",
     description: "A fresh and delicious black coffee",
     price: 5.0,
   },
   {
-    id: 124,
+    id: "124",
     name: "PBJ",
     description: "Peanut butter jelly time!",
     price: 3.45,
   },
   {
-    id: 125,
+    id: "125",
     name: "Carrot cake",
     description: "Ordering here is a piece of cake",
     price: 6.2,
@@ -20,7 +20,7 @@ const menuItems = [
 ];
 
 export function getItemById(id) {
-  return menuItems.find((el) => el.id === +id);
+  return menuItems.find((el) => el.id === id);
 }
 
 export default menuItems;
