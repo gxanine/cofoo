ba35ec840edeea440603f1ba608a2e0e7ac84785
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 23:52:19 +0100

    Add some customisations to the menu items data

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -5,6 +5,24 @@ const menuItems = [
     description: "A fresh and delicious black coffee",
     price: 5.0,
     image: "123.jpg",
+    customisations: [
+      {
+        name: "Milk",
+        description: "Add some more milk",
+        defaultAmount: 1,
+        priceExtra: 0.5,
+        min: 0,
+        max: 2,
+      },
+      {
+        name: "Sugar",
+        description: "Would like some sugar maybe?",
+        defaultAmount: 0,
+        priceExtra: 0.5,
+        min: 0,
+        max: 10,
+      },
+    ],
   },
   {
     id: "124",
@@ -12,6 +30,24 @@ const menuItems = [
     description: "Peanut butter jelly time!",
     price: 3.45,
     image: "124.jpg",
+    customisations: [
+      {
+        name: "Peanut butter",
+        description: "Everyone loves peanut butter...",
+        defaultAmount: 1,
+        priceExtra: 0.1,
+        min: 1,
+        max: 5,
+      },
+      {
+        name: "Jelly",
+        description: "Oh jelly... My sweet jelly...",
+        defaultAmount: 1,
+        priceExtra: 0.15,
+        min: 0,
+        max: 5,
+      },
+    ],
   },
   {
     id: "125",
@@ -19,6 +55,16 @@ const menuItems = [
     description: "Ordering here is a piece of cake",
     price: 6.2,
     image: "125.jpg",
+    customisations: [
+      {
+        name: "Whipped cream",
+        description: "Sweet and delicate clouds for your dessert",
+        defaultAmount: 1,
+        priceExtra: 0.05,
+        min: 0,
+        max: 5,
+      },
+    ],
   },
 ];
 
