21a03a59ceb4f029ac16860463559e900892563b
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 05:03:57 +0100

    Add disabled styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -7,7 +7,8 @@ function Button({ children, danger, ...props }) {
       className={classNames(
         "rounded",
         "text-white px-10 py-3",
-        danger ? "bg-red-500 hover:bg-red-600 active:bg-red-700" : "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700"
+        danger ? "bg-red-500 hover:bg-red-600 active:bg-red-700" : "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700",
+        "disabled:opacity-40 disabled:cursor-not-allowed"
       )}
     >
       {children}
