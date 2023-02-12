d510bb93497f0701ae9bdcb5bb72b3835d4a79dd
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 20:56:31 +0100

    Add the CartOverlay to the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Outlet, useNavigate } from "react-router-dom";
+import CartOverlay from "./components/CartOverlay";
+import {useCart} from "./context/CartContext";
 
 function App() {
   const navigate = useNavigate();
+  const cart = useCart();
   return (
-    <div className="">
+    <div className="h-screen flex flex-col">
       <div className="text-center font-extralight text-8xl py-5 cursor-pointer" onClick={() => navigate("/")}>cofoo</div>
-      <div className="max-w-xl mx-auto">
-          <Outlet />
+      <div className="max-w-xl w-full flex-1 mx-auto pb-80">
+        <div className="flex flex-col">
+          <div className="flex-1">
+            <Outlet />
+          </div>
+          <div className="flex-1">
+
+          </div>
+        </div>
+      </div>
+      <div className="fixed bottom-0 w-full bg-white">
+          <CartOverlay cart={cart} />
       </div>
     </div>
   );
