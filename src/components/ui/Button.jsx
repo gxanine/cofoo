f3a09de41329dad0d445e241534d6848f771d51f
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 15:48:12 +0100

    Add a small variant of Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,19 +1,20 @@
 import { classNames } from "../../utils/classes";
 
-function Button({ children, danger, neutral, ...props }) {
+function Button({ children, danger, neutral, small, ...props }) {
   return (
     <button
       {...props}
       className={classNames(
         "rounded",
         "text-white",
-        "px-5 py-3",
+        small ? "px-2 py-1" : "px-5 py-3",
         danger
           ? "bg-red-500 hover:bg-red-600 active:bg-red-700"
           : neutral
           ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
           : "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700",
-        "disabled:opacity-40 disabled:cursor-not-allowed"
+        "disabled:opacity-40 disabled:cursor-not-allowed",
+        props.className
       )}
     >
       {children}
