744ff859535c9a752d64628a3c006011306b2e3f
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 11:25:52 +0100

    Add neutral style to the Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,14 +1,18 @@
 import { classNames } from "../../utils/classes";
 
-function Button({ children, danger, ...props }) {
+function Button({ children, danger, neutral, ...props }) {
   return (
     <button
       {...props}
       className={classNames(
         "rounded",
-        danger ? "bg-red-500 hover:bg-red-600 active:bg-red-700" : "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700",
         "text-white",
         "px-5 py-3",
+        danger
+          ? "bg-red-500 hover:bg-red-600 active:bg-red-700"
+          : neutral
+          ? "bg-gray-500 hover:bg-gray-600 active:bg-gray-700"
+          : "bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700",
         "disabled:opacity-40 disabled:cursor-not-allowed"
       )}
     >
