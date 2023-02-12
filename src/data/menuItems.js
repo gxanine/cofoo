9f9d51a08fc07fd08f124a4e2559e6cae6f8bddb
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 17:27:55 +0100

    Add images to items

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -4,18 +4,21 @@ const menuItems = [
     name: "Coffee",
     description: "A fresh and delicious black coffee",
     price: 5.0,
+    image: "123.jpg",
   },
   {
     id: "124",
     name: "PBJ",
     description: "Peanut butter jelly time!",
     price: 3.45,
+    image: "124.jpg",
   },
   {
     id: "125",
     name: "Carrot cake",
     description: "Ordering here is a piece of cake",
     price: 6.2,
+    image: "125.jpg",
   },
 ];
 
