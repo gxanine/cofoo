ef2525cc943945450ee81796ffc73e6901586881
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-12 17:29:32 +0100

    Improve ItemDetail functionality and looks

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,45 +1,47 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useCart, useCartDispatch } from "../context/CartContext";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { getItemById } from "../data/menuItems";
+import { classNames } from "../utils/classes";
+import CartAdder from "./CartAdder";
 import Button from "./ui/Button";
 
 function ItemDetail() {
   const { id } = useParams();
 
-  const { name, description, price } = getItemById(id) ?? {};
+  const { name, description, price, image } = getItemById(id) ?? {};
 
   const navigate = useNavigate();
-  const cartDispatch = useCartDispatch();
-  const cart = useCart();
-  const existsInCart = cart.find((i) => i.itemId === id) ? true : false;
+  const location = useLocation();
 
-  function addButtonHandler() {
-    cartDispatch({
-      type: "added",
-      itemId: id,
-      qty: 1,
-    });
-  }
-  function decreaseButtonHandler() {
-    cartDispatch({
-      type: "removed",
-      itemId: id,
-      qty: 1,
-    });
-  }
+  function goBackHandler() {
+    // If previous was home, simply go back
+    // to keep the history clean
+    if (location.state?.from === "/")
+      return navigate(-1);
 
-  function deleteAllHandler() {
-    cartDispatch({
-      type: "deleted",
-      id: cart.find((i) => i.itemId === id).id,
-    });
+    navigate("/", { replace: true });
   }
 
   return (
-    <div className="flex flex-col">
-      <Button neutral onClick={() => navigate(-1)}>
-        ← Back to menu
-      </Button>
+    <div className="flex flex-col gap-5">
+      <div>
+        <Button small neutral onClick={goBackHandler}>
+          ← Back to menu
+        </Button>
+      </div>
+      <div
+        className={classNames(
+          "h-60 w-full",
+          "shadow-inner opacity-80",
+          "rounded-lg",
+          "overflow-hidden",
+          "shadow-lg"
+        )}
+      >
+        <img
+          className="object-cover h-full w-full"
+          src={"/item-photos/" + image}
+        />
+      </div>
       <div className="flex text-3xl gap-2">
         <div className="font-bold">{name}</div>
         <div className="opacity-20">(#{id})</div>
@@ -48,31 +50,25 @@ function ItemDetail() {
       <div className="opacity-50">{description}</div>
       <div>
         <div className="flex justify-center my-5 gap-2">
-          <Button
-            disabled={!existsInCart}
-            danger
-            onClick={decreaseButtonHandler}
-          >
-            － Remove
-          </Button>
-          <Button onClick={addButtonHandler}>＋ Add to cart</Button>
+          <CartAdder itemId={id} />
         </div>
-        {existsInCart && (
-          <div className="opacity-30 text-center">
-            <div>
-              (Already in cart: {cart.find((i) => i.itemId === id)?.qty ?? 0})
-            </div>
-            <button
-              className="text-red-600 hover:underline"
-              onClick={deleteAllHandler}
-            >
-              delete all
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
 }
 
 export default ItemDetail;
+
+// {existsInCart && (
+//   <div className="opacity-30 text-center">
+//     <div>
+//       (Already in cart: {cart.find((i) => i.itemId === id)?.qty ?? 0})
+//     </div>
+//     <button
+//       className="text-red-600 hover:underline"
+//       onClick={deleteAllHandler}
+//     >
+//       delete all
+//     </button>
+//   </div>
+// )}
