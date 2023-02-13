60e1082c58254d29615d741b6dbcb424385b1d60
Grzegorz Ilnicki <greg@dagoly.com>
2023-02-13 09:32:25 +0100

    Reformat with prittier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { Outlet } from "react-router-dom";
 import CartOverlay from "./components/CartOverlay";
-import {useCart} from "./context/CartContext";
-import {classNames} from "./utils/classes";
+import { useCart } from "./context/CartContext";
+import { classNames } from "./utils/classes";
 
 function App() {
   const cart = useCart();
   return (
     <div className="h-screen flex flex-col">
-      <div className={classNames("max-w-xl w-full flex-1",
-      "mx-auto pt-10 pb-32",
-      "flex flex-col")}>
-            <Outlet />
+      <div
+        className={classNames(
+          "max-w-xl w-full flex-1",
+          "mx-auto pt-10 pb-32",
+          "flex flex-col"
+        )}
+      >
+        <Outlet />
       </div>
       <div className="fixed bottom-0 w-full bg-white">
-          <CartOverlay cart={cart} />
+        <CartOverlay cart={cart} />
       </div>
     </div>
   );
